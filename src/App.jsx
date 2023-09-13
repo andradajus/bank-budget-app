255b0814936f4f26f39953683924a0aaf858d581
andradajus <andradajustinmarcus@gmail.com>
2023-09-13 18:52:11 +0800

    Tentative Authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,36 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import LoginForm from './components/FormLogin.jsx'
 import NavBar from './components/Navbar.jsx'
 import RegistrationForm from './components/FormRegistration.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [currentPage, setCurrentPage] = useState('home');
-  const goToRegistrationPage = () => { setCurrentPage('registration');
-  };
+  useEffect(() => {
+    const accounts = [
+      {
+        username: 'admin',
+        password: 'admin',
+      }
+    ]
+
+    localStorage.setItem('accounts', JSON.stringify(accounts))
+  }, [])
+
+  const [currentPage, setCurrentPage] = useState('registration');
 
   return (
-    <>
     <div className="container">
       <header>Bank Mo</header>
+      <NavBar />
         <main>
+        {currentPage === 'registration' && ( <RegistrationForm /> )}
         <h1 className="text-3xl font-bold underline">
       Hello world!
         </h1>
-<<<<<<< HEAD
-        {currentPage === 'registration' && ( <FormRegistration /> )}
-        
-=======
-        {currentPage === 'registration' && ( <RegistrationForm /> )}
->>>>>>> 2cf0ec55efd0c1525a367d8dbb231d8f67bd81ba
           <LoginForm />
-          <NavBar />
-          
         </main>      
       <footer>Copyright 2023</footer>
     </div>   
-    </>
   )
 }
 
